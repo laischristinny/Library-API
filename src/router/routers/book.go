f37bb626c53fb35f58a37a709e5fee36744399ae
Laís Christinny <laiscss201@gmail.com>
2022-08-11 17:34:41 -0300

    Add routers

--- src/router/routers/book.go
+++ src/router/routers/book.go
@@ -1,4 +1,39 @@
 package routers
 
+import (
+	"LibraryAPI-GitFlow/src/controller"
+	"net/http"
+)
+
 var BooksRouters = []Router{
+	{
+		URI:                "/books",
+		Method:             http.MethodPost,
+		Function:             controller.CreateBook,
+	
+	},
+	{
+		URI:                "/books/{bookID}",
+		Method:             http.MethodGet,
+		Function:             controller.SearchBook,
+	
+	},
+	{
+		URI:                "/book",
+		Method:             http.MethodGet,
+		Function:             controller.GetAllBooks,
+	
+	},
+	{
+		URI:                "/book/checkout/{bookID}",
+		Method:             http.MethodPatch,
+		Function:             controller.CheckoutBook,
+		
+	},
+	{
+		URI:                "/books/return/{bookID}",
+		Method:             http.MethodPatch,
+		Function:             controller.ReturnBook,
+		
+	},
 }
